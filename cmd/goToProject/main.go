f0bff1cb43c6dea45f80b3a852f9f3c0c222a7b4
Spirin Mihail <misha_spirin_90@mail.ru>
2023-09-28 16:20:50 +0300

    Arkitail18: add auth

--- cmd/goToProject/main.go
+++ cmd/goToProject/main.go
@@ -1,18 +1,31 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
+
+	auth "project/internal/delivery"
 
 	"github.com/gorilla/mux"
 )
 
 func main() {
-	r := mux.NewRouter()
+	var secret string
+	flag.StringVar(&secret, "secret", "", "secret for jwt encoding")
+	flag.Parse()
+	if secret == "" {
+		flag.Usage()
+		return
+	}
 
-	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Привет, мир!")
-	})
+	r := mux.NewRouter()
+	authHandler := auth.NewAuthHandler(secret)
+	apiPath := "/api/v1"
+	r.HandleFunc(apiPath+"/auth", authHandler.CheckAuth).Methods("GET")
+	r.HandleFunc(apiPath+"/login", authHandler.Login).Methods("POST")
+	r.HandleFunc(apiPath+"/signup", authHandler.Signup).Methods("POST")
+	r.HandleFunc(apiPath+"/logout", authHandler.Logout).Methods("Delete")
 
 	fmt.Println("Server is running on :8080")
 	err := http.ListenAndServe(":8080", r)
---
+++ internal/delivery/login_test.go
@@ -0,0 +1,106 @@
+package auth
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func CreateAuthBody(login string, password string) (res []byte) {
+	authRequest := &AuthUser{login, password}
+	res, _ = json.Marshal(authRequest)
+	return
+}
+func TestLoginPositives(t *testing.T) {
+	positiveTests := map[string]struct {
+		login         string
+		password      string
+		addedLogin    string
+		addedPassword string
+		expectedBody  string
+		expectedCode  int
+	}{
+		"correct credentials": {
+			login:         "abc",
+			password:      "1234",
+			addedLogin:    "abc",
+			addedPassword: "1234",
+			expectedBody:  `{"error":""}`,
+			expectedCode:  http.StatusOK,
+		}}
+	for name, test := range positiveTests {
+		test := test
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			login := test.login
+			password := test.password
+			jsonBody := CreateAuthBody(login, password)
+			reader := bytes.NewReader(jsonBody)
+			req, err := http.NewRequest("POST", "/login", reader)
+			assert.NoError(t, err)
+
+			rr := httptest.NewRecorder()
+			handler := NewAuthHandler("fdsjhfsidfsd")
+			assert.NoError(t, handler.storage.AddUser(test.addedLogin, test.addedPassword))
+			loginHandler := http.HandlerFunc(handler.Login)
+
+			loginHandler.ServeHTTP(rr, req)
+			assert.Equal(t, rr.Code, test.expectedCode, "Http response has wrong status")
+			assert.Equal(t, rr.Body.String(), test.expectedBody, "Json response is wrong.")
+		})
+	}
+}
+
+func TestLoginNegatives(t *testing.T) {
+	positiveTests := map[string]struct {
+		login         string
+		password      string
+		addedLogin    string
+		addedPassword string
+		expectedBody  string
+		expectedCode  int
+	}{
+		"user doesnt exist": {
+			login:         "abc",
+			password:      "1234",
+			addedLogin:    "trq",
+			addedPassword: "fjssd",
+			expectedBody:  `{"error":"user with this name doesnt exist"}`,
+			expectedCode:  http.StatusUnauthorized,
+		},
+		"wrong password": {
+			login:         "abc",
+			password:      "1234",
+			addedLogin:    "abc",
+			addedPassword: "123456789",
+			expectedBody:  `{"error":"wrong password"}`,
+			expectedCode:  http.StatusUnauthorized,
+		},
+	}
+	for name, test := range positiveTests {
+		test := test
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			login := test.login
+			password := test.password
+
+			jsonBody := CreateAuthBody(login, password)
+			reader := bytes.NewReader(jsonBody)
+			req, err := http.NewRequest("POST", "/login", reader)
+			assert.NoError(t, err)
+
+			rr := httptest.NewRecorder()
+			handler := NewAuthHandler("fdsjhfsidfsd")
+			assert.NoError(t, handler.storage.AddUser(test.addedLogin, test.addedPassword))
+			loginHandler := http.HandlerFunc(handler.Login)
+
+			loginHandler.ServeHTTP(rr, req)
+			assert.Equal(t, rr.Code, test.expectedCode, "Http response has wrong status")
+			assert.Equal(t, rr.Body.String(), test.expectedBody, "Json response is wrong.")
+		})
+	}
+}
