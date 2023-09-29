0819b2a6f31e056691849092c2fa0e77005b803a
Kirill Lukyanov <kirill.l.2004@yandex.ru>
2023-09-30 01:13:54 +0300

    Arkitail19: GET and POST handlers for places were added

--- internal/delivery/places_test.go
+++ internal/delivery/places_test.go
@@ -1,8 +1,9 @@
-package delivery
+package handler
 
 import (
 	"bytes"
 	"encoding/json"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -13,6 +14,7 @@ func TestCreatePlace(t *testing.T) {
 	defer ts.Close()
 
 	newPlace := Place{
+		ID:          "1",
 		Name:        "Новое место",
 		Description: "Описание нового места",
 		Rating:      4.0,
@@ -37,17 +39,20 @@ func TestCreatePlace(t *testing.T) {
 }
 
 func TestGetPlaces(t *testing.T) {
+    // Initialize some sample data in the 'places' map
+    places = map[string]Place{
+        "1": {
+            ID:          "1",
+            Name:        "Новое место",
+            Description: "Описание нового места",
+            Rating:      4.0,
+            Cost:        "дешево",
+            ImageURL:    "https://example.com/image.jpg",
+        },
+    }
+
     ts := httptest.NewServer(http.HandlerFunc(GetPlaces))
     defer ts.Close()
-
-    newPlace := Place{
-        Name:        "Новое место",
-        Description: "Описание нового места",
-        Rating:      4.0,
-        Cost:        "дешево",
-        ImageURL:    "https://example.com/image.jpg",
-    }
-    places[newPlace.ID] = newPlace
 
     resp, err := http.Get(ts.URL)
     if err != nil {
@@ -58,5 +63,19 @@ func TestGetPlaces(t *testing.T) {
     if resp.StatusCode != http.StatusOK {
         t.Errorf("Ожидался статус код %d, но получен %d", http.StatusOK, resp.StatusCode)
     }
+
+    body, err := io.ReadAll(resp.Body)
+    if err != nil {
+        t.Fatalf("Ошибка чтения тела ответа: %v", err)
+    }
+
+    expectedResponse := `[{"id":"1","name":"Новое место","description":"Описание нового места","rating":4,"cost":"дешево","imageUrl":"https://example.com/image.jpg"}]
+`
+
+    if string(body) != expectedResponse {
+        t.Errorf("Ожидался ответ:\n%s\n\nПолучен ответ:\n%s", expectedResponse, string(body))
+    }
 }
+
+
 
