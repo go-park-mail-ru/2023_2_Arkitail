aff5ac9aed66c18eb80d226bab5b6d30711d982a
Kirill Lukyanov <kirill.l.2004@yandex.ru>
2023-09-30 00:51:02 +0300

    add object for get test

--- handler/delivery/places_test.go
+++ handler/delivery/places_test.go
@@ -1,15 +1,43 @@
 package delivery
 
 import (
-    "bytes"
-    "encoding/json"
-    "net/http"
-    "net/http/httptest"
-    "testing"
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
 )
 
 func TestCreatePlace(t *testing.T) {
-    ts := httptest.NewServer(http.HandlerFunc(CreatePlace))
+	ts := httptest.NewServer(http.HandlerFunc(CreatePlace))
+	defer ts.Close()
+
+	newPlace := Place{
+		Name:        "Новое место",
+		Description: "Описание нового места",
+		Rating:      4.0,
+		Cost:        "дешево",
+		ImageURL:    "https://example.com/image.jpg",
+	}
+
+	jsonData, err := json.Marshal(newPlace)
+	if err != nil {
+		t.Fatalf("Ошибка кодирования JSON: %v", err)
+	}
+
+	resp, err := http.Post(ts.URL, "application/json", bytes.NewBuffer(jsonData))
+	if err != nil {
+		t.Fatalf("Ошибка отправки POST-запроса: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusCreated {
+		t.Errorf("Ожидался статус код %d, но получен %d", http.StatusCreated, resp.StatusCode)
+	}
+}
+
+func TestGetPlaces(t *testing.T) {
+    ts := httptest.NewServer(http.HandlerFunc(GetPlaces))
     defer ts.Close()
 
     newPlace := Place{
@@ -19,26 +47,7 @@ func TestCreatePlace(t *testing.T) {
         Cost:        "дешево",
         ImageURL:    "https://example.com/image.jpg",
     }
-
-    jsonData, err := json.Marshal(newPlace)
-    if err != nil {
-        t.Fatalf("Ошибка кодирования JSON: %v", err)
-    }
-
-    resp, err := http.Post(ts.URL, "application/json", bytes.NewBuffer(jsonData))
-    if err != nil {
-        t.Fatalf("Ошибка отправки POST-запроса: %v", err)
-    }
-    defer resp.Body.Close()
-
-    if resp.StatusCode != http.StatusCreated {
-        t.Errorf("Ожидался статус код %d, но получен %d", http.StatusCreated, resp.StatusCode)
-    }
-}
-
-func TestGetPlaces(t *testing.T) {
-    ts := httptest.NewServer(http.HandlerFunc(GetPlaces))
-    defer ts.Close()
+    places[newPlace.ID] = newPlace
 
     resp, err := http.Get(ts.URL)
     if err != nil {
@@ -50,3 +59,4 @@ func TestGetPlaces(t *testing.T) {
         t.Errorf("Ожидался статус код %d, но получен %d", http.StatusOK, resp.StatusCode)
     }
 }
+
