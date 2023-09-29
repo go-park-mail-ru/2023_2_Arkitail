cba414c13c62614e89864f5295898e0cd69586ae
Kirill Lukyanov <kirill.l.2004@yandex.ru>
2023-09-29 23:59:22 +0300

    parse by files and add tests

--- cmd/goToProject/main.go
+++ cmd/goToProject/main.go
@@ -1,63 +1,23 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"project/handler/delivery"
 
 	"github.com/gorilla/mux"
 )
 
-type Place struct {
-	ID          string  `json:"id"`
-	Name        string  `json:"name"`
-	Description string  `json:"description"`
-	Rating      float64 `json:"rating"`
-	Cost        string  `json:"cost"`
-	ImageURL    string  `json:"imageUrl"`
-}
-
-var places = map[string]Place{}
-
-func createPlace(w http.ResponseWriter, r *http.Request) {
-	var newPlace Place
-	err := json.NewDecoder(r.Body).Decode(&newPlace)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-	newPlace.ID = "some-unique-id"
-	places[newPlace.ID] = newPlace
-	w.WriteHeader(http.StatusCreated)
-}
-
-func getPlaces(w http.ResponseWriter, r *http.Request) {
-	placeList := []Place{}
-	for _, place := range places {
-		placeList = append(placeList, place)
-	}
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(placeList)
-}
-
 func main() {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Привет, мир!")
-	})
-	
-	r.HandleFunc("/places", createPlace).Methods("POST")
-	r.HandleFunc("/places", getPlaces).Methods("GET")
-
-	r.HandleFunc("/api/data", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Это ваше API!")
-	})
+	r.HandleFunc("/api/v1/places", delivery.CreatePlace).Methods("POST")
+	r.HandleFunc("/api/v1/places", delivery.GetPlaces).Methods("GET")
 
 	fmt.Println("Server is running on :8080")
 	err := http.ListenAndServe(":8080", r)
 	if err != nil {
 		fmt.Println(err)
 	}
-
 }
