df52a9f54994dc522aed666bceadfd98063b3fa5
Spirin Mihail <misha_spirin_90@mail.ru>
2023-10-02 16:20:22 +0300

    Arkitail21: change available methods

--- internal/router/router.go
+++ internal/router/router.go
@@ -1,13 +1,15 @@
 package router
 
 import (
+	"net/http"
+
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
 func AddCors(router *mux.Router, originNames []string) {
 	credentials := handlers.AllowCredentials()
-	methods := handlers.AllowedMethods([]string{"POST"})
+	methods := handlers.AllowedMethods([]string{http.MethodGet, http.MethodPut, http.MethodDelete, http.MethodPost})
 	handlers.MaxAge(3600)
 	origins := handlers.AllowedOrigins(originNames)
 	handlers.CORS(credentials, methods, origins)(router)
