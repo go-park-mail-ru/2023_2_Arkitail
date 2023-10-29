4306285eceeb2cacc98cdf60ef530556ae57c895
Spirin Mihail <misha_spirin_90@mail.ru>
2023-10-29 16:32:01 +0300

    add creation_date

--- reviews/model/review_model.go
+++ reviews/model/review_model.go
@@ -1,9 +1,12 @@
 package model
 
+import "github.com/jackc/pgx/pgtype"
+
 type Review struct {
-	ID      uint   `json:"id`
-	UserId  uint   `json:"user_id"`
-	PlaceId uint   `json:"place_id"`
-	Content string `json:"content"`
-	Rating  uint   `json:"rating"`
+	ID           uint        `json:"id`
+	UserId       uint        `json:"user_id"`
+	PlaceId      uint        `json:"place_id"`
+	Content      string      `json:"content"`
+	Rating       uint        `json:"rating"`
+	CreationDate pgtype.Date `json:"creation_date"`
 }
