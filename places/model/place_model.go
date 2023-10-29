32150b93c2888b1d3dcd22ef895b1096a8f3414f
Spirin Mihail <misha_spirin_90@mail.ru>
2023-10-29 19:25:57 +0300

    add omitempty to place

--- places/model/place_model.go
+++ places/model/place_model.go
@@ -2,7 +2,7 @@ package model
 
 type Place struct {
 	ID          string  `json:"id"`
-	Name        string  `json:"name"`
+	Name        string  `json:"name,omitempty"`
 	Description string  `json:"description,omitempty"`
 	Rating      float64 `json:"rating"`
 	Cost        string  `json:"cost,omitempty"`
