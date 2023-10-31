8b644a6a83f7ef5e395dcd0d8519806f7385f0e3
Spirin Mihail <misha_spirin_90@mail.ru>
2023-10-31 17:10:30 +0300

    change db request

--- places/model/place_model.go
+++ places/model/place_model.go
@@ -6,5 +6,12 @@ type Place struct {
 	Description string   `json:"description,omitempty"`
 	Rating      *float64 `json:"rating,omitempty"`
 	Cost        string   `json:"cost,omitempty"`
+	Adress      string   `json:"adress,omitempty"`
+	WebSite     string   `json:"web-site,omitempty"`
+	Email       string   `json:"email,omitempty"`
+	PhoneNumber string   `json:"phone_number,omitempty"`
+	ReviewCount uint     `json:"review_count,omitempty"`
+	OpenTime    string   `json:"open_hour,omitempty"`
+	CloseTime   string   `json:"close_hour,omitempty"`
 	ImageURL    string   `json:"imageUrl,omitempty"`
 }
--- places/repo/place_repository.go
+++ places/repo/place_repository.go
@@ -38,7 +38,8 @@ func (r *PlaceRepository) AddPlace(place *model.Place) error {
 
 func (r *PlaceRepository) GetPlaces() (map[string]*model.Place, error) {
 	places := make(map[string]*model.Place)
-	rows, err := r.DB.Query("SELECT id, name, description, cost, image_url, (select avg(rating) from review where review.place_id = place.id) as rating FROM place")
+	rows, err := r.DB.Query("SELECT id, name, description, cost, image_url, (select avg(rating) from review where review.place_id = place.id) as rating," +
+		"adress, open_time, close_time, web_site, email, phone_number, (select count(id) from review where place.id = place_id) as review_count FROM place")
 	if err != nil {
 		return nil, err
 	}
@@ -46,10 +47,16 @@ func (r *PlaceRepository) GetPlaces() (map[string]*model.Place, error) {
 	for rows.Next() {
 		place := &model.Place{}
 		rating := sql.NullFloat64{}
-		err = rows.Scan(&place.ID, &place.Name, &place.Description, &place.Cost, &place.ImageURL, &rating)
+		openTime := sql.NullTime{}
+		closeTime := sql.NullTime{}
+		err = rows.Scan(&place.ID, &place.Name, &place.Description, &place.Cost, &place.ImageURL, &rating, &place.Adress, &openTime, &closeTime, &place.WebSite, &place.Email, &place.PhoneNumber, &place.ReviewCount)
 		if rating.Valid {
 			rating.Float64 = math.Floor(rating.Float64*100) / 100
 			place.Rating = &rating.Float64
+		}
+		if openTime.Valid && closeTime.Valid {
+			place.OpenTime = openTime.Time.Format("3:4")
+			place.CloseTime = closeTime.Time.Format("3:4")
 		}
 		if err != nil {
 			return nil, err
