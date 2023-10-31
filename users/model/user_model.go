1d9e1ac438c8dbfce71caeed21decdd5d8da1f01
Spirin Mihail <misha_spirin_90@mail.ru>
2023-10-31 22:21:38 +0300

    change camel case in json

--- users/model/user_model.go
+++ users/model/user_model.go
@@ -11,5 +11,5 @@ type User struct {
 	Name      string      `json:"name,omitempty"`
 	BirthDate pgtype.Date `json:"birth_date"`
 	About     string      `json:"about,omitempty"`
-	AvatarUrl string      `json:"avatarUrl,omitempty"`
+	AvatarUrl string      `json:"avatar_url,omitempty"`
 }
